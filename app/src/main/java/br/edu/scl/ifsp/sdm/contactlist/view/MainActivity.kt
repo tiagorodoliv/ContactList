ab430a50e43bf99336379ec9e2ae6f43da8722ef
Tiago Silas <tiago.rod.oliv@gmail.com>
2023-11-18 17:00:08 -0300

    Contact add

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -2,15 +2,37 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    //Data source
+    private val contactList: MutableList<Contact> = mutableListOf()
+
+    //Adapte
+    private val contactAdapter:ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+    }
+
+    private fun fillContacts() {
+        for (i in 1..50) {
+            contactList.add(
+                Contact(
+                    i,
+                    "Nome $i",
+                    "Endereco $i",
+                    "(16)3333-${i + 1}${i + 2}$i${i + 4}",
+                    "teste@teste$i"
+                )
+            )
+        }
     }
 }
